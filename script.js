1b460c1c4e5fa0762e2806772ac556ec0f54b3dd
kurylukdev <kurylukdev@gmail.com>
2022-06-15 22:52:20 -0300

    javascript clase 2

---
+++ script.js
@@ -0,0 +1,53 @@
+
+/*
+let number1, number2
+let repeat = true
+while(repeat){
+    number1 = parseFloat(prompt("Ingrese un numero"));
+    number2 = parseFloat(prompt("Ingrese otro numero"));
+
+    if(!(isNaN(number1) || isNaN(number2)) ) {
+        repeat = false
+    }
+
+}
+console.log(number1 + number2)
+
+*/
+/*
+let palabra
+do {
+     palabra = prompt("Ingrese una palabra").toLowerCase()
+} while(palabra != "salir") */
+/*
+let numero2, numero1, operacion
+
+do{
+    numero1 = parseFloat(prompt("Ingrese un numero"));
+    numero2 = parseFloat(prompt("Ingrese otro numero"));
+    operacion = prompt("Ingrese una operacion (+,-,%,*")
+}while((isNaN(numero1) || isNaN(numero2)))
+
+switch(operacion){
+    case "+":
+        alert("El resultado es: " + numero1 + numero2)
+        break
+    case "-":
+        alert(numero1 - numero2)
+        break
+    case "/":
+        alert(numero1 / numero2)
+        break
+    case "*":
+        alert(numero1 * numero2)
+        break
+    default:
+        alert("Operacion no valida")
+}*/
+
+
+function sumar() {
+    console.log(5+5)
+}
+
+sumar()
