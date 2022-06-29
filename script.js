6d2644bdc58a94a30c5fbb19d600054fa684a5a3
kurylukdev <kurylukdev@gmail.com>
2022-06-29 08:41:07 -0300

    array object

--- script.js
+++ script.js
@@ -1,20 +1,87 @@
-class Animal{
-    constructor(id, nombre, especie, color, peso){
-        this.id = id
-        this.nombre = nombre
-        this.especie = especie
-        this.color = color
-        this.peso = peso
+let catalogo
+//Comidas
+let papasFritas = 250
+let cocacola = 350
+let hamburguesa = 850
+//Ropa
+let remera = 550
+let pantalon = 1250
+let buzo = 850
+//Herramientas
+let martillo = 500
+let destornillador = 350
+let taladro = 2850
+
+//Inicio de codigo
+
+    do{  
+        catalogo = parseFloat(prompt("Consultar stock del catalogo \n Ingrese numero de opcion: \n 1-Comidas  \n 2-Ropa \n 3-Herramientas \n Recuerde Ingresar un numero valido."))
     }
-}
+    while(isNaN(catalogo)) {
+        
+        switch(catalogo){
+            case 1:
+                class Comida{
+                    constructor(id, producto, precio, stock){
+                        this.id = id
+                        this.producto = producto
+                        this.precio = precio
+                        this.stock = stock
+                    }
+                }
+                
+                const comida1 = new Comida(1, "Papas Fritas", 250, 999 + "/uds")
+                const comida2 = new Comida(2, "Coca-Cola", 350, 999 + "/uds")
+                const comida3 = new Comida(3, "Hamburguesa", 850, 999 + "/uds")
+                
+                const comidas = [comida1, comida2, comida3]
+                
+                comidas.forEach(comida => {
+                    console.table(comida)
+                })
+                break;
 
-const animal1 = new Animal(1, "Crespuculo", "Wolf", "Black", 60)
-const animal2 = new Animal(2, "Bad Bunny BB", "Bunny", "Grey", 10)
-const animal3 = new Animal(3, "Mota", "Beard", "Gris", 460)
-const animal4 = new Animal(4, "Gominola", "Beard", "Brown", 360)
+                case 2:
+                    class Ropa{
+                        constructor(id, producto, precio, stock){
+                            this.id = id
+                            this.producto = producto
+                            this.precio = precio
+                            this.stock = stock
+                        }
+                    }
+                    
+                    const ropa1 = new Ropa(1, "Remera", 550, 999 + "/uds")
+                    const ropa2 = new Ropa(2, "Pantalon", 1250, 999 + "/uds")
+                    const ropa3 = new Ropa(3, "Buzo", 850, 999 + "/uds")
+                    
+                    const ropas = [ropa1, ropa2, ropa3]
+                    
+                    ropas.forEach(ropa => {
+                        console.table(ropa)
+                    }) 
+                    break
+                    case 3:
+                        class Herramientas{
+                            constructor(id, producto, precio, stock){
+                                this.id = id
+                                this.producto = producto
+                                this.precio = precio
+                                this.stock = stock
+                            }
+                        }
+                        
+                        const herramienta1 = new Herramientas(1, "Martillo", 500, 999 + "/uds")
+                        const herramienta2 = new Herramientas(2, "Destornillador", 350, 999 + "/uds")
+                        const herramienta3 = new Herramientas(3, "Taladro", 2850, 999 + "/uds")
+                        
+                        const herramientas = [herramienta1, herramienta2, herramienta3]
+                        
+                        herramientas.forEach(herramienta => {
+                            console.table(herramienta)
+                        })   
+                        break
+                        default: alert("Por favor ingrese una opción valida")
+            }
 
-const animales = [animal1, animal2, animal3, animal4]
-
-animales.forEach(animal => {
-    console.table(animal)
-})
+    }
