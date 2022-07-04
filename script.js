bf53654a2419ef6456e85a2cf10e18605b79a57d
kurylukdev <kurylukdev@gmail.com>
2022-07-04 16:59:09 -0300

    cambios

--- script.js
+++ script.js
@@ -2,6 +2,40 @@ console.log(document.getElementById("prf1"))
 const parrafo4 = document.getElementById("prf4")
 parrafo4.innerText = "hola coder"
 
+class Persona {
+    constructor(id, nombre, apellido, edad, sueldo) {
+        this.id = id
+        this.nombre = nombre
+        this.apellido = apellido
+        this.edad = edad
+        this.sueldo = sueldo
+    }
+}
+
+const persona1 = new Persona(1, "Francisco", "Pugh", 38, 12000)
+const persona2 = new Persona(2, "Dario", "Britos", 20, 22000)
+const persona3 = new Persona(3, "Alejandro", "Santos", 19, 17000)
+const persona4 = new Persona(4, "Gaston", "Sosa", 21, 13000)
+const persona5 = new Persona(5, "Maria", "Parodi", 19, 24000)
+
+const personas = [persona1, persona2, persona3, persona4, persona5]
+
+const divPersonas = document.getElementById('divPersonas')
+
+personas.forEach(persona => {
+    divPersonas.innerHTML += `
+        <div id="persona"${persona.id}>
+            <p>Nombre: ${persona.nombre} </p>
+            <p>Apellido: ${persona.apellido} </p>
+            <p>Edad: ${persona.edad} </p>
+            <p>Sueldo: ${persona.sueldo} </p>
+        </div>
+    
+    `
+})
+
+
+
 /*
 let descuento = 200
 let productos
