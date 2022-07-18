9f38af3b97e08e06d1542ec45ac72369a267dd56
kurylukdev <kurylukdev@gmail.com>
2022-07-18 15:06:43 -0300

    evento tarabjo

--- script.js
+++ script.js
@@ -1,116 +1,34 @@
-let catalogo
-//Comidas
-let papasFritas = 250
-let cocacola = 350
-let hamburguesa = 850
-//Ropa
-let remera = 550
-let pantalon = 1250
-let buzo = 850
-//Herramientas
-let martillo = 500
-let destornillador = 350
-let taladro = 2850
+//array
+class Producto {
+  constructor(id, producto, stock, precio) {
+      this.id = id
+      this.producto = producto
+      this.stock = stock
+      this.precio = precio
+  }
+}
+//array fin
+const formulario = document.getElementById('idForm')
+const productoNuevo = []
+let id = 1;
 
-//Inicio de codigo
+formulario.addEventListener('submit', (event) => {
+  event.preventDefault()
+  let producto = document.getElementById("producto").value
+  let precio = document.getElementById("precio").value
+  let stock = document.getElementById("stock").value
+  const product = new Producto(id, producto, stock, precio)
+  productoNuevo.push(product)
+  console.table(productoNuevo)
+  //Codigo HTML
+  productoNuevo.forEach(producto => {
+    contenedor.innerHTML += `
+    <div class="contenedorProducto" id="comida${producto.id}"> 
+    <h3>${producto.producto}</h3>
+    <p>Precio: ${producto.precio} </p>
+    <p>Stock: ${producto.stock} </p>
+    </div>`
+  }) 
+  formulario.reset()
 
-    do{  
-        catalogo = parseFloat(prompt("Consultar stock del catalogo \n Ingrese numero de opcion: \n 1-Comidas  \n 2-Ropa \n 3-Herramientas \n Recuerde Ingresar un numero valido."))
-    }
-    while(isNaN(catalogo)) {
-        
-        switch(catalogo){   
-            case 1:
-                class Comida{
-                    constructor(id, producto, precio, stock){
-                        this.id = id
-                        this.producto = producto
-                        this.precio = precio
-                        this.stock = stock
-                    }
-                }
-                
-                const comida1 = new Comida(1, "Papas Fritas", 250, 999 + "/uds")
-                const comida2 = new Comida(2, "Coca-Cola", 350, 999 + "/uds")
-                const comida3 = new Comida(3, "Hamburguesa", 850, 999 + "/uds")
-                
-                const comidas = [comida1, comida2, comida3]
-
-                const contenedorComida = document.getElementById('contenedor')
-
-
-                comidas.forEach(comida => {
-                    contenedorComida.innerHTML += `
-                    <div class="styleComida" id="comida${comida.id}"> 
-                    <h3>COMIDAS STOCK</h3>
-                    <p>Nombre: ${comida.producto} </p> 
-                    <p>Apellido: ${comida.precio} </p>
-                    <p>Edad: ${comida.stock} </p>
-                    </div>`
-                    console.table(comida)
-                })
-                break;
-
-                case 2:
-                    class Ropa{
-                        constructor(id, producto, precio, stock){
-                            this.id = id
-                            this.producto = producto
-                            this.precio = precio
-                            this.stock = stock
-                        }
-                    }
-                    
-                    const ropa1 = new Ropa(1, "Remera", 550, 999 + "/uds")
-                    const ropa2 = new Ropa(2, "Pantalon", 1250, 999 + "/uds")
-                    const ropa3 = new Ropa(3, "Buzo", 850, 999 + "/uds")
-                    
-                    const ropas = [ropa1, ropa2, ropa3]
-                    
-                const contenedorRopa = document.getElementById('contenedor')
-
-                ropas.forEach(ropa => {
-                    contenedorRopa.innerHTML += `
-                    <div class="styleRopa" id="comida${ropa.id}"> 
-                    <h3>ROPA STOCK</h3>
-                    <p>Nombre: ${ropa.producto} </p> 
-                    <p>Apellido: ${ropa.precio} </p>
-                    <p>Edad: ${ropa.stock} </p>    
-                    </div>`
-                    console.table(ropa)
-                })
-                    break
-                    case 3:
-                        class Herramientas{
-                            constructor(id, producto, precio, stock){
-                                this.id = id
-                                this.producto = producto
-                                this.precio = precio
-                                this.stock = stock
-                            }
-                        }
-                        
-                        const herramienta1 = new Herramientas(1, "Martillo", 500, 999 + "/uds")
-                        const herramienta2 = new Herramientas(2, "Destornillador", 350, 999 + "/uds")
-                        const herramienta3 = new Herramientas(3, "Taladro", 2850, 999 + "/uds")
-                        
-                        const herramientas = [herramienta1, herramienta2, herramienta3]
-                                            
-                const contenedorHerramientas = document.getElementById('contenedor')
-
-
-                herramientas.forEach(herramienta => {
-                    contenedorHerramientas.innerHTML += `
-                    <div class="styleHerramienta" id="comida${herramienta.id}"> 
-                    <h3>ROPA STOCK</h3>
-                    <p>Nombre: ${herramienta.producto} </p> 
-                    <p>Apellido: ${herramienta.precio} </p>
-                    <p>Edad: ${herramienta.stock} </p>
-                    </div>`
-                    console.table(herramienta)
-                }) 
-                        break
-                        default: alert("Por favor ingrese una opción valida")
-            }
-
-    }
+})
