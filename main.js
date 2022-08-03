ddb163f31615477009d6717884b2fa18589551b1
kurylukdev <kurylukdev@gmail.com>
2022-08-03 18:55:59 -0300

    agregado de api nasa

--- main.js
+++ main.js
@@ -1,96 +1,58 @@
-function Quiz (questions) {
-	this.score = 0; 
-	this.questions = questions;
-	this.questionIndex = 0;
-}
+const dolar = document.querySelector("#dolar")
 
-Quiz.prototype.getQuestionIndex = function () {
-	return this.questions[this.questionIndex]
-}
+//API DOLAR
 
-Quiz.prototype.isEnded = function() {
-	return this.questions.length === this.questionIndex;
-}
+fetch("https://criptoya.com/api/dolar")
+    .then(response => response.json())
+    .then(({oficial, solidario, blue, ccb, mep, ccl}) => {
+        dolar.innerHTML = `<div class="contenido">
+		<p>Blue: <span>$${blue}</span> </p>
+		<p>Oficial: <span>$${oficial}</span> </p>
+		<p>Solidario: <span>$${solidario}</span> </p>
+		<p>Mep: <span>$${mep}</span> </p>
+		<p>CCL: <span>$${ccl}</span> </p>
+		<p>CCB: <span>$${ccb}</span> </p></div>
+        `
+    })
+setInterval(() => {
+    fetch("https://criptoya.com/api/dolar")
+    .then(response => response.json())
+    .then(({oficial, solidario, blue, ccb, mep, ccl}) => {
+        dolar.innerHTML = `<div class="contenido">
+		<p>Blue: <span>$${blue}</span> </p>
+		<p>Oficial: <span>$${oficial}</span> </p>
+		<p>Solidario: <span>$${solidario}</span> </p>
+		<p>Mep: <span>$${mep}</span> </p>
+		<p>CCL: <span>$${ccl}</span> </p>
+		<p>CCB: <span>$${ccb}</span> </p></div>
+        `
+    })
+}, 30000)
 
-Quiz.prototype.guess = function(answer) {
+//API NASA
+//Probe otra forma que encontre en internet y me gustaria saber tu opinion sobre esta forma
+//ami me parecio mas rentable ya que podes cambiar la api desde la Const y no tenes que ir a fetch
 
-	if (this.getQuestionIndex().correctAnswer(answer)) {
-		this.score++;
-	}
+window.addEventListener("load", obtenerDatos) //para que cargue la api apenas cargue la pagina
 
-	this.questionIndex++;
-}
+//Prueba con funcion de =>
+function obtenerDatos(){
+	const apiKey = "AG37wIVmwCdcaK7diiGf9wphlfryY3j93dPekAjh" //const de apikey
 
+	const ruta = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}` // ruta de la api + apiKey
 
-function Question (text, choices, answer) {
-	this.text = text;
-	this.choices = choices;
-	this.answer = answer;
-}
+	const nasa = document.querySelector("#nasa")
 
-Question.prototype.correctAnswer = function(choice) {
-	return choice === this.answer;
-}
+	fetch(ruta)
+	.then(response => response.json())
+	.then(({date, explanation, url,title}) => {
+		nasa.innerHTML = `
+		<h2>${title}</h2>
+		<p>${explanation}</p>
+		<img src="${url}"></img>
+		<p>${date}</p>
+		`
+	})
+} 
+//Fin API NASA
 
-
-function populate() {
-	if (quiz.isEnded()) {
-		showScores();
-	} else {
-		let element = document.getElementById('question');
-		element.innerHTML = quiz.getQuestionIndex().text;
-
-		//show choices
-		let choices = quiz.getQuestionIndex().choices;
-		for (var i = 0; i < choices.length; i++) {
-			let element = document.getElementById('choice' + i);
-			element.innerHTML = choices[i];
-
-			guess("btn" + i, choices[i]);
-		}
-
-		showProgress();
-	}
-}
-
-
-function guess(id, guess) {
-	let button = document.getElementById(id);
-	button.onclick = function() {
-		quiz.guess(guess);
-		populate();
-	}
-}
-
-
-function showProgress() {
-	let currentQuestionNumber = quiz.questionIndex + 1;
-	let element = document.getElementById('progress');
-	element.innerHTML = "Questions " + currentQuestionNumber + " of " + quiz.questions.length;
-}
-
-
-function showScores() {
-	let gameOverHTML = "<h1>Result</h1>"; 
-	gameOverHTML += "<h2 id='score'> Your Scores: " + quiz.score + "</h2>";
-	let element = document.getElementById('quiz');
-	element.innerHTML = gameOverHTML;
-}
-
-
-const questions = [
-	new Question("¿En Que Episodio Murio Obi-Wan Kenobi?", ["Episodio III", "Episodio VI", "Episodio IV", "Episodio V"], "Episodio IV"),
-	new Question("¿Quien era el maestro del Conde Dooku", ["Tyranus", "Darth Sidiou", "Yoda", "Qui-Gon Jinn"], "Yoda"),
-	new Question("¿Quien mato a Qui-Gon Jinn?", ["Darth Maul", "Darth Tyranus", "Darth Sidiou", "Darth Vader"], "Darth Maul"),
-	new Question("Name of the prime Intelligence org during Soviet era?", ["FSB", "CIA", "Gestapo", "KGB"], "KGB"),
-	new Question("Name of the prime Intelligence org during Soviet era?", ["FSB", "CIA", "Gestapo", "KGB"], "KGB"),
-	new Question("Name of the prime Intelligence org during Soviet era?", ["FSB", "CIA", "Gestapo", "KGB"], "KGB"),
-	new Question("Name of the prime Intelligence org during Soviet era?", ["FSB", "CIA", "Gestapo", "KGB"], "KGB"),
-	new Question("Name of the prime Intelligence org during Soviet era?", ["FSB", "CIA", "Gestapo", "KGB"], "KGB"),
-	new Question("Name of the prime Intelligence org during Soviet era?", ["FSB", "CIA", "Gestapo", "KGB"], "KGB"),
-	new Question("¿Cual es el planeta natal de Obi-Wan Kenobi?", ["Stewjon", "Tatooine", "Naboo", "Utapau"], "Stewjon"),
-];
-
-let quiz = new Quiz (questions);
-
-populate();
